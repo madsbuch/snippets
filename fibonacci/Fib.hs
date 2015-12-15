0b47bdbf64b8420340bafd0c3f55f84f50c945a9
Mads Buch <madspbuch@gmail.com>
2015-12-15 21:28:24 +0100

    spelling error

--- fibonacci/Fib.hs
+++ fibonacci/Fib.hs
@@ -2,13 +2,13 @@ module Main where
 
 import System.Environment
 
--- Standard recursive implementaiton, very slow
+-- Standard recursive implementation, very slow
 fib :: Integer -> Integer
 fib 0 = 0
 fib 1 = 1
 fib n = fib (n - 1) + fib (n - 2)
 
--- Using an accumulator
+-- Using an accumulator, tail recursion
 fibAcc :: Integer -> Integer
 fibAcc n = doFib n (0,1)
     where
@@ -16,8 +16,7 @@ fibAcc n = doFib n (0,1)
         doFib 0 (a, b) = a
         doFib n (a, b) = doFib (n-1) (a+b, a)
 
---main = putStrLn $ show (fibAcc 100000)
-
+-- First argument is read and parsed as Integer
 main = do
     a <- getArgs
     putStrLn $ show (fibAcc $ read (a !! 0) )
