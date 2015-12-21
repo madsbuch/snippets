77895c86b6ce4f0751a1ae7d72157f72bd7386b9
Mads Buch <madspbuch@gmail.com>
2015-12-21 13:23:10 +0100

    ADT fib added

--- fibonacci/Fib.hs
+++ fibonacci/Fib.hs
@@ -18,15 +18,44 @@ fibAcc n = doFib n (0,1)
 
 fibCps :: Integer -> Integer
 fibCps n = fibCpsDo n (\x->x)
-	where
-		fibCpsDo :: Integer -> (Integer -> Integer) -> Integer
-		fibCpsDo 0 k = k 0
-		fibCpsDo 1 k = k 1
-		fibCpsDo n k = fibCpsDo (n-1) (\a 
-			-> fibCpsDo (n-2) (\b 
-				-> k (a+b)))
+    where
+        fibCpsDo :: Integer -> (Integer -> Integer) -> Integer
+        fibCpsDo 0 k = k 0
+        fibCpsDo 1 k = k 1
+        fibCpsDo n k = fibCpsDo (n-1) (\a 
+            -> fibCpsDo (n-2) (\b 
+                -> k (a+b)))
 
 -- First argument is read and parsed as Integer
 main = do
     a <- getArgs
     putStrLn $ show (fibAcc $ read (a !! 0) )
+
+{-
+ Fibonacci on structures. More precisely, Peano arithmetic implemented using ADTs
+-}
+
+data Nat = Z | S Nat deriving Show
+
+s10 = (S (S (S (S (S (S (S (S (S (S Z))))))))))
+
+showNat :: Nat -> Integer
+showNat Z     = 0
+showNat (S n) = (showNat n) + 1
+
+-- Value level addition
+add :: Nat -> Nat -> Nat
+add Z b      = b
+add (S a) b  = add a (S b)
+
+-- Fibonacci
+adtFib :: Nat -> Nat
+adtFib Z         = Z
+adtFib (S Z)     = (S Z)
+adtFib (S (S n)) = (adtFib (S n)) `add` (adtFib n)
+
+{-
+ Fibonacci through map-reduce
+-}
+
+--mapFoldFib
