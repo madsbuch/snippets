4800e334c87ebb1b34124c56c4e164d2a4da13f4
Mads Buch <madspbuch@gmail.com>
2016-01-04 22:52:40 +0100

    added conversions

--- fibonacci/fib.pl
+++ fibonacci/fib.pl
@@ -14,23 +14,33 @@ nat(0).
 nat(s(_)).
 
 % Readable nats
-read_nat(0, 0).
-read_nat(s(N), R) :-
-	read_nat(N, R1),
+nat_to_int(0, 0).
+nat_to_int(s(N), R) :-
+	nat_to_int(N, R1),
 	R is R1 + 1.
 
-parse_nat(0, 0).
-parse_nat(X, s(R)) :-
+int_to_nat(0, 0).
+int_to_nat(X, s(R)) :-
 	X1 is X-1,
-	parse_nat(X1, R).
+	int_to_nat(X1, R).
 
 % Definition of addition ( add(A, B, Result ))
 add(0, B, B).
 add(s(A), B, R) :- add(A, s(B), R).
-	
+
 fib_peano(0, 0).
 fib_peano(s(0), s(0)).
 fib_peano(s(s(N)), R) :-
 	fib_peano(N, A),
 	fib_peano(s(N), B),
 	add(A, B, R).
+
+% Wrapper functions dealing with all the conversions
+fib_peano_wrapper_get_res(A, B) :-
+	int_to_nat(A, X),
+	fib_peano(X, Y),
+	nat_to_int(Y, B).
+fib_peano_wrapper_get_arg(A, B) :-
+	int_to_nat(B, Y),
+	fib_peano(X, Y),
+	nat_to_int(X, A).
